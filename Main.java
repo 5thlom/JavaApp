4022cf59f6b8a5348027a5ff7a6612f7c75b9a0f
zhy <chiwa.csci@outlook.com>
2014-11-10 00:06:13 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String args[]){
+		 new CreateDatabase();
+		mainGui frame = new mainGui("Customer Reviews DataBase");
+		frame.setVisible(true);
+		
+	}
+}
